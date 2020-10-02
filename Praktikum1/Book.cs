90a978e75d4e1e0decdd5c60a4eb45cac7eb9740
Fajar Ru <kzofajar@gmail.com>
2020-10-02 08:33:55 +0700

    Latihan 1

---
+++ Praktikum1/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Praktikum1
+{
+    public class Book
+    {
+        public string title { get; set; }
+
+        public string author { get; }
+
+        public Book(string title, string author)
+        {
+            this.title = title;
+            this.author = author;
+        }
+    }
+}
---
+++ Praktikum1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Praktikum1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var book = new Book("Buku Pemrograman", "Anonim");
+
+            Console.WriteLine(book.author);
+            Console.WriteLine(book.title);
+        }
+    }
+}
